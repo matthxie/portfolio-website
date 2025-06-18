a16d7dafe2ebe522968218e8fe67e9c99db5850b
Matthew Xie <mattx180@gmail.com>
2025-06-17 20:16:50 -0400

    make nav bar sticky

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -3,23 +3,36 @@ import Link from 'next/link'
 const navItems = {
   '/projects': {
     name: 'projects',
+    external: false,
   },
-  '/resume': {
-    name: 'experience',
+  'https://github.com/matthxie': {
+    name: 'github',
+    external: true,
   },
-  '/': {
-    name: 'contact',
+  'https://www.linkedin.com/in/mattxie/': {
+    name: 'linkedin',
+    external: true,
   },
 }
 
 export function Navbar() {
   return (
-    <aside className="px-6 md:px-12 mb-16 tracking-tight">
+    <aside className="sticky top-0 z-50 px-6 md:px-12 mb-16 tracking-tight">
       <div className="-ml-[8px] flex justify-end">
         <nav className="flex flex-row items-start" id="nav">
           <div className="flex flex-row space-x-2">
-            {Object.entries(navItems).map(([path, { name }]) => {
-              return (
+            {Object.entries(navItems).map(([path, { name, external }]) => 
+              external ? (
+                <a
+                  key={path}
+                  href={path}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                >
+                  {name}
+                </a>
+              ) : (
                 <Link
                   key={path}
                   href={path}
@@ -28,7 +41,7 @@ export function Navbar() {
                   {name}
                 </Link>
               )
-            })}
+            )}
           </div>
         </nav>
       </div>
